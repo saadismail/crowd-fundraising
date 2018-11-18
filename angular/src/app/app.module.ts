b0134cece44354b515dd55252dcabffdb76434b3
Saad Ismail <saad3112@gmail.com>
2018-11-18 12:03:31 +0500

    add header, footer, list-categories, about-us and service

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+import { AuthGuard } from './guards/auth.guard';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
@@ -13,11 +17,30 @@ import { MyTargetComponent } from './components/my-target/my-target.component';
 import { CreateProjectComponent } from './components/create-project/create-project.component';
 import { ListProjectsComponent } from './components/list-projects/list-projects.component';
 import { IndProjectComponent } from './components/ind-project/ind-project.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ListCategoriesComponent } from './components/list-categories/list-categories.component';
+
+import { AuthService } from './services/auth.service';
+import { AboutUsComponent } from './about-us/about-us.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'forget-password', component: ForgetPasswordComponent },
+  { path: 'profile', component: MyProfileComponent, canActivate:[AuthGuard] },
+  { path: 'target', component: MyTargetComponent, canActivate:[AuthGuard] },
+  { path: 'create-project', component: CreateProjectComponent, canActivate:[AuthGuard] },
+  { path: 'projects', component: ListProjectsComponent},
+  { path: 'project/:id', component: IndProjectComponent},
+  { path: 'categories', component: ListCategoriesComponent},
+  { path: 'about-us', component: AboutUsComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     HomeComponent,
     RegisterComponent,
     LoginComponent,
@@ -26,13 +49,24 @@ import { IndProjectComponent } from './components/ind-project/ind-project.compon
     MyTargetComponent,
     CreateProjectComponent,
     ListProjectsComponent,
-    IndProjectComponent
+    IndProjectComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListCategoriesComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ angular/src/app/services/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  // To Be Implemented
+  isLoggedIn() {
+    return true;
+  }
+}

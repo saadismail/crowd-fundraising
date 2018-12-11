812f70b0bed18c4b61b8cab143ff57b4f420ff26
shehryarnaeem <shehryar.naeem44@gmai.com>
2018-12-11 09:12:33 +0500

    Hello

--- frontend/src/app/admin-login/admin-login.component.ts
+++ frontend/src/app/admin-login/admin-login.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormControl,FormGroup,Validators,FormBuilder} from '@angular/forms';
+import {AuthService} from 'app/auth.service';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-admin-login',
   templateUrl: './admin-login.component.html',
   styleUrls: ['./admin-login.component.css']
 })
 export class AdminLoginComponent implements OnInit {
-
-  constructor() { }
-
+  name;
+  password;
+  adForm;
+  status;
+  constructor(public formBuilder:FormBuilder,private authService:AuthService,private router:Router) { }
   ngOnInit() {
+    this.adForm=this.formBuilder.group({
+      name:['',[Validators.required]],
+      password:['',[Validators.required]]
+    });
+  }
+
+  onClickSubmit(data){
+    this.router.navigate(['/home']);
   }
 
 }
